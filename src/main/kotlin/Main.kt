2330c1f9db23f8316dcf07521655c10d9f05bd4f
Poulad <poulad.pld@gmail.com>
2022-08-13 15:08:25 -0400

    Companion Object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,27 +3,39 @@ import clazzes.Student
 import funs.TextBox
 import funs.fib
 import funs.plus
+import objekts.GeoLocation
+import objekts.Objekt
 
-fun main(args: Array<String>) {
-    log("program started")
-    println("Hello \n  World \t!".replaceMultipleWhitespaceEx())
-    log(logLevel = 3, message = "another log at level 3")
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            log("program started")
+            println("Hello \n  World \t!".replaceMultipleWhitespaceEx())
+            log(logLevel = 3, message = "another log at level 3")
+            // Try adding program arguments via Run/Debug configuration.
+            // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+            println("Program arguments: ${args.joinToString()}")
 
-    val tb = TextBox("tb1") + TextBox("txtbx2")
-    println("Combined is: $tb")
+            val tb = TextBox("tb1") + TextBox("txtbx2")
+            println("Combined is: $tb")
 
-    println("Fibonacci number is: ${fib(10_000)}")
+            println("Fibonacci number is: ${fib(10_000)}")
 
-    val student = Student("Joe", "Doe", 123)
-    println(student.getName())
+            val student = Student("Joe", "Doe", 123)
+            println(student.getName())
 
-    val meetingNewYork = Meeting(546, "NYC", 111111111111)
-    val meetingToronto = meetingNewYork.copy(location = "Toronto")
-    println("Meeting in NYC ${meetingNewYork.hashCode()} $meetingNewYork")
-    println("Meeting in Toronto ${meetingToronto.hashCode()} $meetingToronto")
+            val meetingNewYork = Meeting(546, "NYC", 111111111111)
+            val meetingToronto = meetingNewYork.copy(location = "Toronto")
+            println("Meeting in NYC ${meetingNewYork.hashCode()} $meetingNewYork")
+            println("Meeting in Toronto ${meetingToronto.hashCode()} $meetingToronto")
+
+            Objekt.MyNumbers.add(43)
+            Objekt.printMe()
+
+            println("GeoLocation is ${GeoLocation.of(34.123, -79.0054)}")
+        }
+    }
 }
 
 fun log(message: String, logLevel: Int = 2): Boolean {
---
+++ src/main/kotlin/objekts/Objekt.kt
@@ -0,0 +1,30 @@
+package objekts
+
+import java.io.Closeable
+
+object Objekt {
+    const val Name = "singleton1"
+    val MyNumbers = arrayListOf<Int>()
+
+    fun printMe() {
+        println("object $Name with numbers $MyNumbers")
+    }
+}
+
+class GeoLocation private constructor(lat: Double, lon: Double) {
+    private val latitude: Double = lat
+    private val longitude: Double = lon
+
+    override fun toString() = "( $latitude , $longitude )"
+
+    companion object : Closeable {
+        fun of(lat: Double, lon: Double): GeoLocation {
+            // TODO: validate range of input params
+            return GeoLocation(lat, lon)
+        }
+
+        override fun close() {
+            println("closed!")
+        }
+    }
+}
