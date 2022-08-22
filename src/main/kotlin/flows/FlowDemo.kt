09d45a8f7198ca42c3cdf663020f142da990daff
Poulad <poulad.pld@gmail.com>
2022-08-22 06:31:34 -0400

    Flows

---
+++ src/main/kotlin/flows/FlowDemo.kt
@@ -0,0 +1,29 @@
+package flows
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.math.pow
+
+fun main(args: Array<String>): Unit = runBlocking {
+    val flow = startFlowing()
+    launch { startCollectingValues(flow) }
+    launch { startCollectingValues(flow, "x") }
+}
+
+fun startFlowing() = flow<String> {
+    println("Emitting values from thread ${Thread.currentThread().name}")
+    (1..20)
+        .filter { it % 3 == 0 }
+        .map { Pair(it, it.toDouble().pow(2.0).toInt()) }
+        .map { "${it.first} squared is ${it.second}" }
+        .forEach { emit(it) }
+}
+
+suspend fun startCollectingValues(strsFlow: Flow<String>, collectorId: String = ""): Unit {
+    val id = "COLLECTOR[${collectorId.ifEmpty { "?" }}]"
+    strsFlow.collect {
+        println("$id -> $it")
+    }
+}
